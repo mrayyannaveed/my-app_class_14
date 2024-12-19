15d6061095c827f3a6397d0b7e7220759d8737aa
Rayyan Naveed <164235692+mrayyannaveed@users.noreply.github.com>
2024-12-20 00:16:54 +0500

    remove commit

--- src/app/[todoId]/loading.tsx
+++ src/app/[todoId]/loading.tsx
@@ -1,5 +1,5 @@
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
+
     return (
         <div className="h-screen flex justify-center items-center">
         <h1 className="lg:text-7xl xl:text-8xl text-4xl sm:text-5xl md:text-6xl font-bold">Data Fetching</h1>
